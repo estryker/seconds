2c02d854fcede084bda80a6e603b05cfe3465f10
Ethan Stryker <e.stryker@gmail.com>
2012-03-23 02:44:44 +0000

    added Rakefile

---
+++ Rakefile.rb
@@ -0,0 +1,23 @@
+require 'rake/testtask'
+require 'rake/rdoctask'
+#require 'rdoc/task'
+
+Rake::RDocTask.new do |rd|
+  rd.main = "README"
+  rd.rdoc_files.include("README","lib/*.rb")
+  rd.options << "--all"
+  rd.options << "--inline-source"
+end
+
+task :upload_docs => [:rdoc] do
+ sh %(scp -r html/* e_stryker@seconds.rubyforge.org:/var/www/gforge-projects/seconds)
+end
+
+task :release => [:rdoc] do 
+  sh %(gem build seconds.gemspec)
+  sh %(git push  gitosis@rubyforge.org:seconds.git master)
+  
+  # god I love ruby:
+  newest_gem = Dir["seconds-*.gem"].sort {|a,b| File.mtime(b) <=> File.mtime(a)}.first
+  sh "gem push #{newest_gem}"
+end
